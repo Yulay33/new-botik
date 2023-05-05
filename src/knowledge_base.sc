f2820d165ea3aa662eb019002dfe8d10d93ec40e
ypetrova723@gmail.com <ypetrova723@gmail.com>
2023-05-05 09:42:50 +0000

    Zip file uploaded

---
+++ src/knowledge_base.sc
@@ -0,0 +1,29 @@
+theme: /
+    # мини база знаний
+   
+    state: ProtPos || noContext = true
+        q!: эй
+        q!: * {прот* * посу*} *
+        a: Инструкция по протирке посуды. Ссылка: https://coda.io/d/_dHHq7vmGxH9/_suakS
+        inlineButtons:
+            {text:"Вернуться к Чек-листу", callback_data: "repit_step"}
+    
+    state: ProtPrib || noContext = true
+        q!: * {прот* * прибор*} *
+        a: Инструкция по протирке приборов. Ссылка: https://coda.io/d/_dHHq7vmGxH9/_suakS     
+        inlineButtons:
+            {text:"Вернуться к Чек-листу", callback_data: "repit_step"}
+        
+    state: ProtPover || noContext = true
+        q!: * {прот* * (повер*|стол*)} *
+        a: Инструкция по протирке поверхностей. Ссылка: https://coda.io/d/_dHHq7vmGxH9/_suakS     
+        inlineButtons:
+            {text:"Вернуться к Чек-листу", callback_data: "repit_step"}
+        
+    state: InstAll
+        a: Инструкция по ... Ссылка: https://coda.io/d/_dHHq7vmGxH9/_suakS     
+        inlineButtons:
+            {text:"Вернуться к Чек-листу", callback_data: "repit_step"}  
+            
+            
+            
---
+++ src/utils.js
@@ -0,0 +1,54 @@
+function prepareForChecklist() {
+    // объявление переменных и присвоение им значений, для более короткого обращения к ним
+    
+    // Объект для сохранения любых сессионных данных. 
+    // После завершения всех реакций бота структура $session сохраняется во внутренней базе данных. 
+    // При начале новой сессии все данные будут обнулены.
+    // внутри js файлов обращение к переменно осуществляется через $jsapi.context()
+    var session = $jsapi.context().session;
+    
+    // Объект для сохранения постоянных данных о клиенте. После завершения всех реакций бота структура $client сохраняется во внутренней базе данных. Не имеет особых полей.
+    // внутри js файлов обращение к переменно осуществляется через $jsapi.context()
+    var client = $jsapi.context().client;
+    
+    // Переменная $request содержит данные запроса клиента.
+    // внутри js файлов обращение к переменно осуществляется через $jsapi.context()
+    // Объект запроса в исходном виде для последующей обработки в сценарии.
+    var request = $jsapi.context().request.rawRequest;
+    // указание id клиента
+    session.clientChatId = request.message.chat.id
+    // установка таймзоны Москва для напоминаний-будильников
+    $reactions.setClientTimezone("Europe/Moscow");
+    // установка английского языка для функции  moment, которая в боте получает название дня недели
+    moment.locale('en');
+    // указание ника пользователя
+    client.username = request.message.from.username;
+    if (client.username === undefined){
+       client.username = "nonickname"; 
+    }else{
+       client.username = "@"+client.username; 
+    }
+    // указание имени пользователя  
+    client.name = request.message.from.first_name;
+    // id чата с клиентом
+    client.id = request.message.from.id
+    // создание массива под отчет
+    client.report = [];
+    // шаг начала цикла по смене
+    session.stepNumber = 0;
+    // счетчик ошибок установлен в 1, чтобы при первом запуске корректно отрабатывал стейт /Checklist/Start/Error 
+    session.ErrorCounter = 1;
+    // переменная для запоминания id сообщения, которое нужно будет удалить при срабаытывании напомнинаия (придет такое же новое)
+    session.early_id!="";
+    
+    // // переменная для запоминания текста сообщения, которое нужно будет удалить при срабаытывании напомнинаия (придет такое же новое)
+    session.early_text!="";
+    // день недели для словаря
+    session.checklistType = moment($jsapi.dateForZone("Europe/Moscow", "YYYY-MM-dd")).format("dddd");
+    
+    // шаг выполнения чек-листа
+//    session.stepNumber = 0;    // дубль
+    // количество возникших трудностей по ходу выполнения шага чек-листа
+//    session.ErrorCounter = 0;  // дубль
+
+}
